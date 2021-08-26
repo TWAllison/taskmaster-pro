83dc4a6753bdfd521927e7ce841f959e005ee427
TWAllison <tylerallison55@gmail.com>
2021-08-26 00:08:41 -0600

    added Bootstrap cards and modal form-control

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -112,6 +112,37 @@ $(".list-group").on("click", "p", function() {
 
   });
 
+  $(".list-group").on("blur", "input[type='text']", function() {
+
+    // get current text
+    var date = $(this)
+    .val()
+    .trim();
+
+    ///get the parent ul's id attribute
+    var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+    // get the tasks position in the list of other list elements
+    var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+    // update task in the array and resave to localStorage
+    tasks[status][index].date = date;
+    saveTasks();
+
+    // recreate span element with boostrap classes
+    var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+    //replace input with span 
+    $(this).replaceWith(taskSpan);
+  });
+
   
 
 
